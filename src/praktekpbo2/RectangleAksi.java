78a8716e7d3bf942d252b8301245f0fd750d67fe
AdrianMochtar <mochtaradrian.am@gmail.com>
2018-03-31 12:51:01 +0800

    praktek2

---
+++ src/praktekpbo2/RectangleAksi.java
@@ -0,0 +1,19 @@
+package praktekpbo2;
+public class RectangleAksi {
+    public static void main(String[] args){
+        
+        Rectangle r1 = new Rectangle();
+        r1.panjang =9;
+        r1.lebar =3;
+        
+        r1.cetakinfo();
+        System.out.println("Luas Rectangle = "+r1.hitungLuas());
+        r1.cetakLuas();
+        
+        Rectangle r2 = new Rectangle();
+        r2.cetakinfo();
+        
+        Rectangle r3 = new Rectangle (50,30);
+        r3.cetakinfo();   
+    }
+}
